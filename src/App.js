e89528bbb8204720b45b7b3f0eca0eb20b5f3814
Steven Calderon <scalderon1504@gmail.com>
2022-07-08 12:25:56 -0500

    Pruebas demo jest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import * as math from './math.js';
+export const doAdd      = (a, b) => math.add(a, b);
+export const doSubtract = (a, b) => math.subtract(a, b);
+export const doMultiply = (a, b) => math.multiply(a, b);
+export const doDivide   = (a, b) => math.divide(a, b);
 function App() {
   return (
     <div className="App">
--- src/mock_jest_mock.test.js
+++ src/mock_jest_mock.test.js
@@ -1,14 +1,11 @@
 import * as app from "./App";
 import * as math from "./math";
-
-math.add = jest.fn();
-math.subtract = jest.fn();
-
+// Set all module functions to jest.fn
+jest.mock("./math.js");
 test("calls math.add", () => {
   app.doAdd(1, 2);
   expect(math.add).toHaveBeenCalledWith(1, 2);
 });
-
 test("calls math.subtract", () => {
   app.doSubtract(1, 2);
   expect(math.subtract).toHaveBeenCalledWith(1, 2);
